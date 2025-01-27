2159410abba738fd8e23495ea48bd2855b1e2db7
kylegontjes <kgontjes@umich.edu>
2025-01-27 11:38:00 -0500

    update package documentation

--- R/asr.R
+++ R/asr.R
@@ -1,5 +1,5 @@
 asr <- function(df,tr,tip_name_var ,pheno,model="ER",node_states = "marginal",conf_threshold=0.875){
-  # Info
+  # Package info
 
   library(tidyverse)
   library(corHMM)
@@ -8,18 +8,19 @@ asr <- function(df,tr,tip_name_var ,pheno,model="ER",node_states = "marginal",co
   corHMM_out = corHMM(phy=tr,data=df[,c(tip_name_var,pheno)],rate.cat = 1,model=model,node.states = node_states)
 
   # Get Parent Child data
-  outcome_str <- df[,pheno] %>% `names<-`(df[[tip_name_var]])
+  outcome_str <- df[,pheno] %>% `names<-`(df[[tip_name_var]]) %>% as.factor
   parent_child_df <- get_parent_child_data(tr=tr,anc_data=corHMM_out$states,pheno_data=outcome_str,conf_threshold = conf_threshold)
 
   # Annotate parent child data
   parent_child_df <- get_phenotypic_continuation_data(parent_child_df)
 
-  return(parent_child_df)
+  asr_output = list(corHMM_out = corHMM_out,parent_child_df = parent_child_df)
+  return(asr_output)
 }
 
 get_parent_child_data <- function(tr,anc_data,pheno_data,conf_threshold=0.875){
   # Tree edge info
-  anc_data = as.data.frame(anc_data) %>% `colnames<-`(c("0","1"))
+  anc_data = as.data.frame(anc_data) %>% `colnames<-`(levels(pheno_data))
   edge <- tr$edge %>% as.data.frame
   colnames(edge) <- c("parent", "child")
 
@@ -36,9 +37,9 @@ get_parent_child_data <- function(tr,anc_data,pheno_data,conf_threshold=0.875){
 
   # Prediction values for the parent & child
   for(i in nodes){
-    edge[edge$parent == i,"parent_mle"] <- anc_data[anc_data$node == i,'1'] *100
+    edge[edge$parent == i,"parent_mle"] <- anc_data[anc_data$node == i,levels(pheno_data)[2]] *100
     edge[edge$parent == i,"parent_val"] <- anc_data[anc_data$node == i,"pred"]
-    edge[edge$child == i,"child_mle"] <- anc_data[anc_data$node == i,'1'] *100
+    edge[edge$child == i,"child_mle"] <- anc_data[anc_data$node == i,levels(pheno_data)[2]] *100
     edge[edge$child == i,"child_val"] <- anc_data[anc_data$node == i,"pred"]
   }
 
