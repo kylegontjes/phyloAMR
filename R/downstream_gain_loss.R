045d55a60b35e5b0e0d19e129dfa1323c20f2e27
kylegontjes <kgontjes@umich.edu>
2025-02-03 12:17:39 -0500

    add initial implementation of downstream gain-loss analyses

---
+++ R/downstream_gain_loss.R
@@ -0,0 +1,80 @@
+downstream_gain_loss <- function(comparitor_parent_child_df,parent_child_df,tr){
+  stretches = get_trait_traces_on_tree(parent_child_df,tr)
+  downstream_changes = get_gain_loss_on_stretches(comparitor_parent_child_df = comparitor_parent_child_df,stretches)
+  return(downstream_changes)
+}
+
+get_trait_traces_on_tree <- function(parent_child_df,tr){
+  all_possible_paths = nodepath(tr)
+
+  gains = parent_child_df %>% subset(gain_any==1) %>% .$child
+  tip_gains = subset(gains,gains < min(tr$edge[1,]))
+  ancestral_gains = subset(gains,!gains %in% tip_gains)
+
+  gain_paths = lapply(all_possible_paths,FUN=function(x,gains){
+    didit = x %in% gains
+    if(sum(didit)==0){
+      string=NA
+    } else {
+      didit_pos = which(didit)
+      string=x[didit_pos:length(x)]
+    }
+
+    return( string)
+  },gains=ancestral_gains) %>% subset(is.na(.)==F)
+
+  trait_found = unique(c(subset(parent_child_df,child_val==1) %>% .$child,subset(parent_child_df,parent_val==1) %>% .$parent))
+
+  paths_w_trait = lapply(gain_paths,FUN=function(x,trait_found){
+    results = x %in% trait_found
+    names(results) = x
+    return(results)
+  },trait_found=trait_found)
+
+  find_true_stretches <- function(x) {
+    rle_result <- rle(x)  # Run-length encoding
+    nodes = names(x)
+    starts <- cumsum(c(1, head(rle_result$lengths, -1)))  # Start positions
+    stretches <- data.frame(
+      startnode =names( starts[rle_result$values]),start = starts[rle_result$values],
+                            end = starts[rle_result$values] + rle_result$lengths[rle_result$values] - 1)
+    paths = list()
+    if(nrow(stretches)>0){
+
+      for(i in 1:nrow(stretches)){
+        start= stretches[i,"start"]
+        end = stretches[i,"end"]
+        paths[[i]] <- as.vector(nodes[start:end])
+      }
+    }
+    paths = unlist(paths)
+
+
+    return(paths)
+  }
+
+  true_stretches = lapply(paths_w_trait,find_true_stretches)
+  return(true_stretches)
+}
+
+
+get_gain_loss_on_stretches = function(comparitor_parent_child_df, stretches){
+  gains_c = comparitor_parent_child_df  %>% subset(gain_any==1) %>% .$child
+  loss_c = comparitor_parent_child_df  %>% subset(loss_any==1) %>% .$child
+
+  get_downstream_nodes = function(stretches){
+    lapply(stretches,FUN=function(x){x[-1]})
+  }
+
+  downstream = get_downstream_nodes(stretches)
+  downstream_positions = stretches %>% unlist %>% unique %>% sort
+  num_downstream = length(downstream_positions)
+  gains = downstream_positions[which(downstream_positions %in%  gains_c)]
+  gains_num = length(gains)
+  gains_prop = gains_num / num_downstream
+  loss =  downstream_positions[which(downstream_positions %in%  loss_c)]
+  loss_num =length(loss)
+  loss_prop = loss_num / num_downstream
+  summary = data.frame(num_downstream=num_downstream,gains_num=gains_num,gains_prop=gains_prop,loss_num=loss_num,loss_prop=loss_prop)
+  return(summary)
+}
