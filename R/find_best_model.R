5e042b13839fc2ed66ecc4867ce7f9a3eba3b093
kylegontjes <kgontjes@umich.edu>
2025-02-21 11:44:12 -0500

    add upper and lower bound and the SYS option for models to the find best model function

--- R/find_best_model.R
+++ R/find_best_model.R
@@ -13,14 +13,14 @@
 #'   }
 #' @export
 
-find_best_model = function(df,tr,tip_name_var,pheno,node_states = "joint"){
+find_best_model = function(df,tr,tip_name_var,pheno,node_states = "joint",upper_bound=1e9,lower_bound=1e-9){
   # Check if phenotype is 0,1
   check_phenotype(df[[pheno]])
 
   # Run corHMM to estimate hidden rates
-  corHMM_ER = corHMM::corHMM(phy=tr,data=df[,c(tip_name_var,pheno)],rate.cat = 1,model="ER",node.states = node_states)
-  corHMM_ARD = corHMM::corHMM(phy=tr,data=df[,c(tip_name_var,pheno)],rate.cat = 1,model="ARD",node.states = node_states)
-  corHMM_SYS = corHMM::corHMM(phy=tr,data=df[,c(tip_name_var,pheno)],rate.cat = 1,model="SYS",node.states = node_states)
+  corHMM_ER = corHMM::corHMM(phy=tr,data=df[,c(tip_name_var,pheno)],rate.cat = 1,model="ER",node.states = node_states,upper.bound=upper_bound,lower.bound=lower_bound)
+  corHMM_ARD = corHMM::corHMM(phy=tr,data=df[,c(tip_name_var,pheno)],rate.cat = 1,model="ARD",node.states = node_states,upper.bound=upper_bound,lower.bound=lower_bound)
+  corHMM_SYS = corHMM::corHMM(phy=tr,data=df[,c(tip_name_var,pheno)],rate.cat = 1,model="SYS",node.states = node_states,upper.bound=upper_bound,lower.bound=lower_bound)
 
   AICS = c("ER"=corHMM_ER$AIC,"ARD"=corHMM_ARD$AIC,"SYS" = corHMM_SYS$AIC) %>% sort
 
--- tests/testthat/test-find_best_model.R
+++ tests/testthat/test-find_best_model.R
@@ -8,4 +8,4 @@ tip_name_var <- "tip_name_var"
 best_model <- phylosuite::find_best_model(df,tr,tip_name_var,pheno,"joint")
 
 expect_type(best_model, "character")
-expect(best_model %in% c("ER","ARD"),"Not in desired model list")
+expect(best_model %in% c("ER","ARD","SYS"),"Not in desired model list")
