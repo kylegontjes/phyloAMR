1c9e1c1140906bbb052f70c6ed930d4ddaa61bf0
kylegontjes <kgontjes@umich.edu>
2025-02-27 16:44:30 -0500

    update cluster detection algorithm to have generic naming, not require the patient_id present, and add checks to function

--- R/asr_cluster_detection.R
+++ R/asr_cluster_detection.R
@@ -2,61 +2,99 @@
 #'
 #' Description of what the function does.
 #'
-#' @param x Description of parameter `x`
-#' @param y Description of parameter `y`
+#' @param df Dataframe with tip name variable and phenotype
+#' @param tr Phylogenetic tree
+#' @param tip_name_var Name of variable containing tip names in df
+#' @param patient_id Name of variable containing patient IDs, can be combined with faux_clusters option to factor into whether a cluster should have >1 patient. (Optional)
+#' @param pheno Name of phenotype variable in df
+#' @param parent_child_df Parent child dataframe from asr() object
+#' @param node_states Whether the reconstruction was "joint" or "marginal"
+#' @param faux_clusters Two options exist: relabel (i.e., specify clusters as having 1pt) or remove (i.e., consider these singletons) (Optional)
+#' @param confidence Whether to use high (i.e., 0 -> 1) or low (0 -> 0.5) confidence transitions when determining clustering. ONLY USAGE FOR MARGINAL STATE RECONSTRUCTIONS
+#' @param remove_revertant Whether to remove revertant episodes from consideration in a cleaned
+#' @param collapse_cluster Whether to create a variable that collapses cluster calls into one category
 #' @return Description of return value
 #' @export
-asr_cluster_detection <- function(df,tr,pheno,parent_child_df,remove_faux="yes",confidence="high",remove_revertant="yes",collapse_cluster="yes"){
+asr_cluster_detection <- function(df,tr,tip_name_var,patient_id=NULL,pheno,parent_child_df,faux_clusters=F,node_states="joint",confidence="high",remove_revertant="yes",collapse_cluster="yes"){
+  # Check if states are as desired
+  check_joint_confidence(node_states,confidence)
+  # Check faux_cluster
+  check_faux_clusters(patient_id,faux_clusters)
+
+  # Set up data frame
+  clustering_data <- data.frame(isolate_no = df[[tip_name_var]])
+  rownames(clustering_data) <- clustering_data$isolate_no
+  # If patient ID is not null
+  if(is.null(patient_id)==F){
+    clustering_data$patient_id = df[[patient_id]]
+  }
+
   # Get clustering data
-  clustering_data <- cbind.data.frame(isolate_no = df$isolate_no,
-                                      Patient_ID = df$Patient_ID,
-                                      asr_cluster = lapply(df$isolate,FUN=get_clustering_data,parent_child_df,confidence) %>% unlist)
-# Clean clusters
-if(remove_faux == "yes"){
-  clustering_data$asr_cluster <- remove_faux_clusters(clustering_data)
-} else {
-  clustering_data$asr_cluster <- relabel_faux_clusters(clustering_data)
-}
+  clustering_data$asr_cluster <- sapply(clustering_data$isolate_no,FUN=get_clustering_data,parent_child_df,confidence)
 
-# Simplify strings
-clustering_data$asr_cluster_renamed <- simplify_acr_clustering_string(string = "asr_cluster",df = clustering_data,tr = tr,remove_faux =remove_faux,remove_revertant = remove_revertant)
-# Simplify to cluster
-if(collapse_cluster=="yes"){
-  clustering_data$asr_cluster_collapsed <- group_by_category(clustering_data$asr_cluster)
-}
+  # Clean clusters
+  if(faux_clusters == "remove"){
+    clustering_data$asr_cluster <- remove_faux_clusters(clustering_data)
+  }
 
-# Get tip data and merge it with the clustering data
-tip_data <- parent_child_df %>% subset(child <= ape::Nnode(tr)+1) %>% dplyr::mutate(isolate_no=child_name)
-tip_data_df <- tip_data %>% dplyr::left_join(.,clustering_data) %>% `rownames<-`(.$isolate_no)
+  if(faux_clusters == "relabel"){
+    clustering_data$asr_cluster <- remove_faux_clusters(clustering_data)
+  }
 
-# List containing all of the results from this pipeline
-results <- list(parent_child_df = parent_child_df,
+  # Simplify strings
+  clustering_data$asr_cluster_renamed <- simplify_acr_clustering_string(string = "asr_cluster",df = clustering_data,tr = tr,faux_clusters =faux_clusters,remove_revertant = remove_revertant)
+
+  # Simplify to cluster
+  if(collapse_cluster=="yes"){
+    clustering_data$asr_cluster_collapsed <- group_by_category(clustering_data$asr_cluster)
+  }
+
+  # Get tip data and merge it with the clustering data
+  tip_data <- parent_child_df %>% subset(child <= ape::Nnode(tr)+1) %>% dplyr::mutate(isolate_no=child_name)
+  tip_data_df <- tip_data %>% dplyr::left_join(.,clustering_data) %>% `rownames<-`(.$isolate_no)
+
+  # List containing all of the results from this pipeline
+  results <- list(parent_child_df = parent_child_df,
                 tip_data_df = tip_data_df)
-return(results)
+  return(results)
 }
+
+check_joint_confidence <- function(node_states,confidence){
+  if(node_states == "joint" & confidence != "high"){
+    stop("When using joint reconstruction states, you must specify confidence as 'high'")
+  }
+}
+
+check_faux_clusters <- function(patient_id,faux_clusters){
+  if(is.null(patient_id) == T & faux_clusters != F){
+    stop("Must have patient id variable for when faux clusters is specified")
+  }
+  if(!faux_clusters %in% c(F,"relabel","remove")){
+    stop("Must specify faux cluster under options of F, relabel, or remove")
+  }
+}
+
 
 get_clustering_data <- function (isolate, edge_data, confidence)
 {
   tip_row <- edge_data %>% subset(child_name == isolate)
   classification <- c()
   if (tip_row$child_val == 0) {
-    classification <- ifelse(tip_row[,paste0("loss_",confidence)] == 1, "s_rev_tip",
+    classification <- ifelse(tip_row[,paste0("loss_",confidence)] == 1, "revertant_tip",
                              "no feature")
   }
   if (tip_row$child_val == 1) {
     classification <- ifelse(tip_row[,paste0("gain_",confidence)] == 1, "singleton",
-                             paste0("r_", get_largest_anc_cluster(tip_row, edge_data) %>%
-                                      .$child))
+                             paste0("cluster_", get_largest_anc_cluster(tip_row, edge_data) %>% {ifelse(.=="root","root",.$child)}))
   }
   if (classification == "singleton") {
-    classification <- paste0("r_", reclassify_singletons(node_data = tip_row,
-                                                         edge_data = edge_data,confidence=confidence))
+    classification <- reclassify_singletons(node_data = tip_row,
+                                                         edge_data = edge_data,confidence=confidence)
   }
   if (classification == "no feature") {
-    classification <- paste0("s_rev_", get_largest_anc_reversion(node_data = tip_row,
-                                                                 edge_data = edge_data,confidence=confidence)) %>% {
-                                                                   ifelse(. == "s_rev_no feature", "no feature", .)
-                                                                 }
+    classification <- get_largest_anc_reversion(node_data = tip_row,
+                                                                 edge_data = edge_data,confidence=confidence)
+    classification <- ifelse(classification == "no feature",classification,paste0("revertant_",classification))
   }
   return(classification)
 }
@@ -70,7 +108,7 @@ get_largest_anc_cluster <- function(node_data,edge_data){
       repeat{
         output <- get_parent_node(output, edge_data)
         if(nrow(output)==0){
-          output <- c("no_0.5_in_path")
+          output <- c("root")
           break
         } else  {if (output$parent_val == 0.5){
           break
@@ -123,24 +161,24 @@ get_parent_node <- function(node_data,edge_data){
 
 
 remove_faux_clusters <- function(cluster_data){
-  cluster_size <- table(cluster_data$asr_cluster) %>% subset(grepl("r_",names(.))) %>% subset(!grepl("r_singleton",names(.)))
+  cluster_size <- table(cluster_data$asr_cluster) %>% subset(grepl("r_",names(.))) %>% subset(!grepl("singleton",names(.)))
   # Isolates where gain at internal node was called, but no other isolate was connected to it
   true_clusters <- subset(cluster_size,cluster_size>1) %>% names
   not_cluster <- names(cluster_size[!names(cluster_size) %in% true_clusters])
   # Clusters with only one patient
   cluster_size_pts <- sapply(true_clusters,FUN=function(x){
-    cluster_data  %>% subset(asr_cluster == x) %>% .$Patient_ID %>% unique %>% length
+    cluster_data  %>% subset(asr_cluster == x) %>% .$patient_id %>% unique %>% length
   })
   not_more_than_one_pt <- subset(cluster_size_pts,cluster_size_pts==1) %>% names(.)
   # Reclassify these isolates as singletons
-  cluster_fin <-  ifelse(cluster_data$asr_cluster  %in% c(not_more_than_one_pt,not_cluster),"r_singleton",cluster_data$asr_cluster)
+  cluster_fin <-  ifelse(cluster_data$asr_cluster  %in% c(not_more_than_one_pt,not_cluster),"singleton",cluster_data$asr_cluster)
 
   return(cluster_fin)
 }
 
 relabel_faux_clusters <- function(cluster_data) {
   cluster_size <- table(cluster_data$asr_cluster) %>% subset(grepl("r_",
-                                                                   names(.))) %>% subset(!grepl("r_singleton", names(.)))
+                                                                   names(.))) %>% subset(!grepl("singleton", names(.)))
 
   # Isolates where gain at internal node was called, but no other isolate was connected to it
   true_clusters <- subset(cluster_size, cluster_size > 1) %>%
@@ -150,37 +188,37 @@ relabel_faux_clusters <- function(cluster_data) {
 
   # Clusters with only one patient
   cluster_size_pts <- sapply(true_clusters, FUN = function(x) {
-    cluster_data %>% subset(asr_cluster == x) %>% .$Patient_ID %>%
+    cluster_data %>% subset(asr_cluster == x) %>% .$patient_id %>%
       unique %>% length
   })
   not_more_than_one_pt <- subset(cluster_size_pts, cluster_size_pts ==
                                    1) %>% names(.)
   # Rename only one isolate as singleton & add _1pt_only label to the faux single patient cluster
-  cluster_fin <- ifelse(cluster_data$asr_cluster %in% not_cluster,"r_singleton",
+  cluster_fin <- ifelse(cluster_data$asr_cluster %in% not_cluster,"singleton",
                         ifelse(cluster_data$asr_cluster %in% not_more_than_one_pt,paste0(cluster_data$asr_cluster,"_1pt_only"),
                                cluster_data$asr_cluster))
 
   return(cluster_fin)
 }
 
-simplify_acr_clustering_string <-  function(string,df,tr,remove_faux,remove_revertant){
+simplify_acr_clustering_string <-  function(string,df,tr,faux_clusters,remove_revertant){
   # add rownames
   rownames(df) <- df$isolate_no
   #Reorder vector to tree plotting
   df_ordered <- df %>% dplyr::select(isolate_no,paste0(string)) %>% .[match(ggtree::get_taxa_name(ggtree::ggtree(tr)),df$isolate_no),]
 
   # Convert Names to Easy to Report String
-  df_ordered$string <- df_ordered %>% dplyr::select(paste0(string)) %>% unlist %>%  {ifelse(. == "r_singleton","Singleton",
+  df_ordered$string <- df_ordered %>% dplyr::select(paste0(string)) %>% unlist %>%  {ifelse(. == "singleton","Singleton",
                                                                                             ifelse(. == "no feature","No Feature",
-                                                                                                   ifelse(.!="s_rev_tip" & grepl("s_rev_",.),"No Feature",
-                                                                                                          ifelse(.=="s_rev_tip","Reversion Tip",.))))}
+                                                                                                   ifelse(.!="revertant_tip" & grepl("revertant",.),"No Feature",
+                                                                                                          ifelse(.=="revertant_tip","Revertant Tip",.))))}
   # Name clusters
   num_clusters <- length(df_ordered$string  %>% subset(.,!. %in% c("No Feature","Singleton","Reversion Tip")) %>% subset(.,grepl("1pt",.)==F) %>% unique)
   if(num_clusters >0){
     cluster_names <- df_ordered$string  %>% subset(.,!. %in% c("No Feature","Singleton","Reversion Tip")) %>% subset(.,grepl("1pt",.)==F) %>% unique %>% `names<-`(paste0("Cluster ",1:length(.))) %>% stats::setNames(names(.), .)
     df_ordered$string <- dplyr::recode(df_ordered$string, !!! cluster_names)
 
-    if(remove_faux=="no"){
+    if(faux_clusters=="rename"){
       # Name faux clusters
       faux_cluster_names <- df_ordered$string %>% subset(.,grepl("1pt",.)==T) %>% unique %>% `names<-`(paste0("Single Patient Cluster ",1:length(.))) %>% stats::setNames(names(.), .)
       df_ordered$string <- dplyr::recode(df_ordered$string, !!! faux_cluster_names)
@@ -204,8 +242,8 @@ simplify_acr_clustering_string <-  function(string,df,tr,remove_faux,remove_reve
 
 group_by_category <- function(string){
   ifelse(string =="no feature","no feature",
-         ifelse(string == "r_singleton","r singleton",
-                ifelse(grepl("r_",string)==T,"r cluster",
-                       ifelse(grepl("s_rev_tip",string)==T,"s revertant tip",
-                              ifelse(grepl("s_rev_",string)==T,"s revertant lineage",NA)))))
+         ifelse(string == "singleton","singleton",
+                ifelse(grepl("cluster_",string)==T,"cluster",
+                       ifelse(grepl("revertant_tip",string)==T,"revertant tip",
+                              ifelse(grepl("revertant_",string)==T,"revertant lineage",NA)))))
 }
