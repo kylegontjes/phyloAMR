d2c89722e6d6d80970f535d58ab41966e1736363
kylegontjes <kgontjes@umich.edu>
2025-02-27 21:09:01 -0500

    add painting function

---
+++ R/paint_tree_with_states.R
@@ -0,0 +1,21 @@
+#' paint_tree_with_states
+#'
+#' Function to paint a tree
+#'
+#' @param parent_child_df Parent child dataframe from asr()
+#' @param tr Phylogenetic tree
+#' @param scale_labels Labels for present and absent
+#' @param legend_name Name for legend
+#' @return Tree with painted states
+#' @export
+paint_tree_with_states <- function(parent_child_df,tr,scale_labels = c("Present","Absent"),legend_name = "Trait"){
+  edge_color <-  scale_color_manual(breaks=c(1,0),values=c("red","black"),labels=scale_labels,name=legend_name)
+  # Get outcome and parent string
+  outcome_str = parent_child_df %>% subset(.,child <= length(tr$tip.label)) %>% arrange(child)  %>% .$child_val
+  parent_str = parent_child_df %>% subset(.,child > length(tr$tip.label)) %>% arrange(child) %>% .$child_val
+  # Get data
+  d = data.frame(node=seq_len(nrow(blbli_asr$parent_child_df)),state=c(outcome_str,parent_str))
+  # Tree
+  painted_tree <- ggtree(tr) %<+% d + aes(color=as.factor(state)) + edge_color
+  return(painted_tree)
+}
