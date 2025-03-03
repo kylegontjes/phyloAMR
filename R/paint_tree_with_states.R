ea440e9c155f00af35d2b914f25f3ba44efe8361
kylegontjes <kgontjes@umich.edu>
2025-03-03 14:09:09 -0500

    make painting function customizable

--- R/paint_tree_with_states.R
+++ R/paint_tree_with_states.R
@@ -1,6 +1,6 @@
 #' paint_tree_with_states
 #'
-#' Function to paint a tree
+#' Function to paint a tree with anestral states
 #'
 #' @param parent_child_df Parent child dataframe from asr()
 #' @param tr Phylogenetic tree
@@ -8,8 +8,8 @@
 #' @param legend_name Name for legend
 #' @return Tree with painted states
 #' @export
-paint_tree_with_states <- function(parent_child_df,tr,scale_labels = c("Present","Absent"),legend_name = "Trait"){
-  edge_color <-  scale_color_manual(breaks=c(1,0),values=c("red","black"),labels=scale_labels,name=legend_name)
+paint_tree_with_states <- function(parent_child_df,tr,scale_breaks=c(1,0),scale_labels = c("Present","Absent"),scale_colors=c("red","black"),legend_name = "Trait"){
+  edge_color <-  scale_color_manual(breaks=scale_breaks,values=scale_colors,labels=scale_labels,name=legend_name)
   # Get outcome and parent string
   outcome_str = parent_child_df %>% subset(.,child <= length(tr$tip.label)) %>% arrange(child)  %>% .$child_val
   parent_str = parent_child_df %>% subset(.,child > length(tr$tip.label)) %>% arrange(child) %>% .$child_val
