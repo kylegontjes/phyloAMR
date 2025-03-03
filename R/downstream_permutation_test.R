07c4f66993fe3dd1674aa7e81fe0e5fd2b18b7e6
kylegontjes <kgontjes@umich.edu>
2025-03-03 16:36:40 -0500

    make downstream permutation applicable for joint and marginal

--- R/downstream_permutation_test.R
+++ R/downstream_permutation_test.R
@@ -7,19 +7,20 @@
 #' @param tr Phylogenetic tree
 #' @param tip_name_var Tip name variable
 #' @param node_states Joint or marginal reconstruction
+#' @param conf_threshold Set value for confidence threshold of MLE calls if marginal
 #' @param confidence Whether to use high or low confidence transition nodes when node_states are marginal
 #' @return Summary stats for downstream gain and loss of a trait
 #' @export
-downstream_permutation_test <- function(comparitor,df,tr,tip_name_var,trait,node_states = "joint",num_permutations=1000,num_cores = 6){
+downstream_permutation_test <- function(comparitor,df,tr,tip_name_var,trait,node_states = "joint",conf_threshold=NULL,confidence=NULL,num_permutations=1000,num_cores = 6){
   library(pbmcapply)
   # trait
-  trait_asr = asr(df=df,tr=tr,tip_name_var=tip_name_var,pheno = trait,model = "ER",node_states = 'joint',conf_threshold = 0.875) %>% .$parent_child_df
+  trait_asr = asr(df=df,tr=tr,tip_name_var=tip_name_var,pheno = trait,model = "ER",node_states = node_states,conf_threshold = conf_threshold) %>% .$parent_child_df
 
   # comparitor
-  comparitor_asr = asr(df=df,tr=tr,tip_name_var=tip_name_var,pheno = comparitor,model = "ER",node_states = 'joint',conf_threshold = 0.875) %>% .$parent_child_df
+  comparitor_asr = asr(df=df,tr=tr,tip_name_var=tip_name_var,pheno = comparitor,model = "ER",node_states = node_states,conf_threshold = conf_threshold) %>% .$parent_child_df
 
   # Get observed
-  trait_downstream_obs = downstream_gain_loss(comparitor_asr,trait_asr,tr)
+  trait_downstream_obs = downstream_gain_loss(comparitor_asr,trait_asr,tr,node_states = node_states,confidence=confidence)
   # permute
   num_isolates = nrow(df)
   num_features = sum(as.numeric(df[[comparitor]]))
@@ -30,7 +31,7 @@ downstream_permutation_test <- function(comparitor,df,tr,tip_name_var,trait,node
   colnames(trait_runs) = permutation_names
   trait_runs=data.frame(trait_runs,tip_name_var = df[,tip_name_var])
   asr_permutation = pbmclapply(permutation_names,FUN=function(x){
-    asr_result = asr(df=trait_runs,tr=tr,tip_name_var="tip_name_var",pheno = x,model = "ER",node_states = 'joint',conf_threshold = 0.875) %>% .$parent_child_df
+    asr_result = asr(df=trait_runs,tr=tr,tip_name_var="tip_name_var",pheno = x,model = "ER",node_states = node_states,conf_threshold = confidence) %>% .$parent_child_df
     downstream_perm = downstream_gain_loss(asr_result,trait_asr,tr)
 
     return(downstream_perm)
