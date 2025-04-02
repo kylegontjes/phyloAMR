9518c5784307260cf4173b2d1283734d646120b9
kylegontjes <kgontjes@umich.edu>
2025-04-01 23:56:25 -0400

    update phyloaware regression by linting

--- R/phyloaware_regression.R
+++ R/phyloaware_regression.R
@@ -3,28 +3,33 @@
 #' This function performs regression on our dataset
 #'
 #' @export
-phyloaware_regression <- function(pheno,variables,df,first_present=NULL,patient_id=NULL,culture_date=NULL,multivariable=NULL,stepwise_direction=NULL,entry_criteria=NULL,retention_criteria=NULL,confounding_criteria=NULL){
+phyloaware_regression <- function(pheno, variables, df, first_present = NULL, patient_id = NULL, culture_date = NULL, multivariable = NULL, stepwise_direction = NULL, entry_criteria = NULL, retention_criteria = NULL, confounding_criteria = NULL) {
   # Get dataset with first isolate
-  datasets= phyloaware_dataset_curation(pheno,df,first_present=first_present,patient_id=patient_id,culture_date=culture_date)
+  datasets <- phyloaware_dataset_curation(pheno, df, first_present = first_present, patient_id = patient_id, culture_date = culture_date)
   # Unadjusted
-  univariable <- lapply(datasets,FUN=function(x){univariable_regression_table(outcome=pheno, dataset=x, variables=variables)})  %>% `names<-`(names(datasets))
-  results <- list(datasets=datasets,univariable=univariable)
+  univariable <- lapply(datasets, FUN = function(x) {
+    univariable_regression_table(outcome = pheno, dataset = x, variables = variables)
+  })  %>% `names<-`(names(datasets))
+  results <- list(datasets = datasets, univariable = univariable)
   # Multivariable
-  if(is.null(multivariable)==TRUE){
-    break
+  if (is.null(multivariable) == TRUE) {
+    stop("No multivariable regression requested")
+    return(results)
   }
-  if(multivariable=="purposeful"){
-    multivariable <- lapply(datasets,FUN=function(x){purposeful_selection_algorithm(outcome=pheno,variables=variables,dataset=x,entry_criteria=entry_criteria,retention_criteria=retention_criteria,confounding_criteria=confounding_criteria)})  %>% `names<-`(names(datasets))
-    results[['multivariable']] <- multivariable
-  } else if(multivariable=='AIC'){
-    multivariable <- lapply(datasets,FUN=function(x){
-      model = as.formula(paste0(pheno," ~ 1 +",paste0(variables,collapse = " + ")))
-      if(stepwise_direction =='forward'){
-        initialize_model <- glm(formula =  as.formula(paste0(pheno," ~ 1")),data=x,family='binomial')
-        stepwise_glm_model <- suppressMessages(stats::step(initialize_model,direction = stepwise_direction, scope =model))
+  if (multivariable == "purposeful") {
+    multivariable <- lapply(datasets, FUN = function(x) {
+      purposeful_selection_algorithm(outcome = pheno, variables = variables, dataset = x, entry_criteria = entry_criteria, retention_criteria = retention_criteria, confounding_criteria = confounding_criteria)
+    })  %>% `names<-`(names(datasets))
+    results[["multivariable"]] <- multivariable
+  } else if (multivariable == "AIC") {
+    multivariable <- lapply(datasets, FUN = function(x) {
+      model <- as.formula(paste0(pheno, " ~ 1 +", paste0(variables, collapse = " + ")))
+      if (stepwise_direction == "forward") {
+        initialize_model <- glm(formula =  as.formula(paste0(pheno, " ~ 1")), data = x, family = "binomial")
+        stepwise_glm_model <- suppressMessages(stats::step(initialize_model, direction = stepwise_direction, scope = model))
       }   else {
-        glm_model <- glm(model, data = x,family = 'binomial')
-        stepwise_glm_model <- suppressMessages(stats::step(glm_model,direction = stepwise_direction))
+        glm_model <- glm(model, data = x, family = "binomial")
+        stepwise_glm_model <- suppressMessages(stats::step(glm_model, direction = stepwise_direction))
       }
       ci <- suppressMessages(confint(stepwise_glm_model))
       final <- cbind(exp(cbind(OR = coef(stepwise_glm_model), ci)) %>% round(., 2),
@@ -34,12 +39,12 @@ phyloaware_regression <- function(pheno,variables,df,first_present=NULL,patient_
         as.data.frame %>% mutate(`OR (95% CI)` = paste0(OR, " (", `2.5%`, "-", `97.5%`, ")")) %>% select(`OR (95% CI)`, p_value)
       return(final)
     }) %>% `names<-`(names(datasets))
-  results[['multivariable']] <- multivariable
-  } else if(multivariable=='pvalue'){
-  multivariable <- lapply(datasets,FUN=function(x){
-    pvalue_informed_regression(outcome=pheno,dataset=x,variables=variables,pvalue_threshold=entry_criteria) %>% .['final_model']
+    results[["multivariable"]] <- multivariable
+  } else if (multivariable == "pvalue") {
+    multivariable <- lapply(datasets, FUN = function(x) {
+      pvalue_informed_regression(outcome = pheno, dataset = x, variables = variables, pvalue_threshold = entry_criteria) %>% .["final_model"]
   }) %>% `names<-`(names(datasets))
-    results[['multivariable']] <- multivariable
+    results[["multivariable"]] <- multivariable
   }
   return(results)
 }
