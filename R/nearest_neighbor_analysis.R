55430a35f369c64b050a6d59e1780c3fb4dce171
kylegontjes <kgontjes@umich.edu>
2025-04-21 16:06:08 -0400

    update coding for nearest neighbor analyses to be linted and streamlined

--- R/nearest_neighbor_analysis.R
+++ R/nearest_neighbor_analysis.R
@@ -4,45 +4,45 @@
 #'
 #' This function will use the phylogenetic tree to identify an isolate's nearest neighbor.
 #' @param comparison_df Dataframe with isolates and their nearest neighbors.
-#' @param cat_vars Categorical variables. Must be in the comparison_df
+#' @param categorical_vars Categorical variables. Must be in the comparison_df
 #' @param binary_vars Binary variables. Must be in the comparison_df AND coded 0 and 1.
-#' @param cont_vars Continuous variables. Must be in the comparison_df
+#' @param continuous_vars Continuous variables. Must be in the comparison_df
 #' @param log_2 Boolean (TRUE/FALSE) Whether to perform log-2 transformation on continuous variables
 #' @return Dataframe with differences in the categorical and continuous variables between an isolate and their nearest neighbor
 #' @export
-nearest_neighbor_analysis <- function(comparison_df, cat_vars = NULL, binary_vars = NULL, cont_vars = NULL, log_2 = FALSE) {
+nearest_neighbor_analysis <- function(comparison_df, categorical_vars = NULL, binary_vars = NULL, continuous_vars = NULL, log_2 = FALSE) {
   # Create comparison dataframe
   comps_df <- cbind.data.frame(isolate_no = comparison_df$isolate_no, nearest_neighbor = comparison_df$nearest_neighbor)
 
   # Categorical differences
-  if (is.null(cat_vars) == FALSE){
-  cat_diff <- lapply(cat_vars, FUN = function(x) {
+  if (is.null(categorical_vars) == FALSE) {
+    cat_diff <- lapply(categorical_vars, FUN = function(x) {
     nn_cat_diff(comparison_df, x)
-  }) %>% do.call(cbind, .) %>% as.data.frame %>% `colnames<-`(paste0(cat_vars, "_cat_diff"))
-  comps_df <- cbind(comps_df,cat_diff)
+  }) %>% do.call(cbind, .) %>% as.data.frame %>% `colnames<-`(paste0(categorical_vars, "_cat_diff"))
+  comps_df <- cbind(comps_df, cat_diff)
   }
 
   # Binary differences
-  if (is.null(binary_vars) == FALSE){
-  binary_diff <- lapply(binary_vars, FUN = function(x) {
+  if (is.null(binary_vars) == FALSE) {
+    binary_diff <- lapply(binary_vars, FUN = function(x) {
     nn_binary_diff(comparison_df, x)
   }) %>% do.call(cbind, .) %>% as.data.frame %>% `colnames<-`(paste0(binary_vars, "_binary_diff"))
-  comps_df <- cbind(comps_df,binary_diff)
+  comps_df <- cbind(comps_df, binary_diff)
   }
 
   # Continuous differences
-  if (is.null(cont_vars) == FALSE){
-  cont_diff <- lapply(cont_vars, FUN = function(x) {
+  if (is.null(continuous_vars) == FALSE) {
+  cont_diff <- lapply(continuous_vars, FUN = function(x) {
     nn_cont_diff(comparison_df, x, log_2 = log_2)
   }) %>% do.call(cbind.data.frame, .)
-  comps_df <- cbind(comps_df,cont_diff)
+  comps_df <- cbind(comps_df, cont_diff)
   }
 
   return(comps_df)
 }
 
 nn_cat_diff <- function(df, var) {
-  cat_diff <- ifelse(df[, var] ==  df[, paste0(var, "_nn")], 'same','different')
+  cat_diff <- ifelse(df[, var] ==  df[, paste0(var, "_nn")], "same", "different")
   return(cat_diff)
 }
 
--- R/nearest_neighbor_summary_statistics.R
+++ R/nearest_neighbor_summary_statistics.R
@@ -3,48 +3,48 @@
 #' This function will generate summary statistics for nearest neighbor comparisons
 #'
 #' @param nn_diff_df Dataframe with isolates and their nearest neighbors, generated from nearest_neighbor_analysis.
-#' @param cat_vars Categorical variables. Must be in the comparison_df
+#' @param continuous_vars Categorical variables. Must be in the comparison_df
 #' @param binary_vars Binary variables. Must be in the comparison_df AND coded 0 and 1.
-#' @param cont_vars Continuous variables. Must be in the comparison_df
+#' @param continuous_vars Continuous variables. Must be in the comparison_df
 #' @param log_2 Boolean (TRUE/FALSE) Whether to perform log-2 transformation on continuous variables
 #' @return Dataframe with differences in the categorical and continuous variables between an isolate and their nearest neighbor
 #' @export
 
-nearest_neighbor_summary_statistics <- function(nn_diff_df, cat_vars = NULL, binary_vars = NULL, cont_vars = NULL, log_2 = FALSE) {
+nearest_neighbor_summary_statistics <- function(nn_diff_df, categorical_vars = NULL, binary_vars = NULL, continuous_vars = NULL, log_2 = FALSE) {
   n <- nrow(nn_diff_df)
   # Create summary dataset
-  comps_sum <- cbind.data.frame(n=n)
+  comps_sum <- cbind.data.frame(n = n)
 
   # Categorical differences
-  if (is.null(cat_vars) == FALSE){
-  cat_vars_2 <- paste0(cat_vars, "_cat_diff")
-  cat_sum_df <- lapply(cat_vars_2, FUN = function(x) {
-    cat_summary(x, nn_diff_df = nn_diff_df, n = n)
-  }) %>% do.call(cbind.data.frame, .)
-  comps_sum <- cbind(comps_sum, cat_sum_df)
+  if (is.null(continuous_vars) == FALSE) {
+    continuous_vars_names <- paste0(continuous_vars, "_cat_diff")
+    cat_sum_df <- lapply(continuous_vars_names, FUN = function(x) {
+      cat_summary(x, nn_diff_df = nn_diff_df, n = n)
+    }) %>% do.call(cbind.data.frame, .)
+    comps_sum <- cbind(comps_sum, cat_sum_df)
   }
 
   # Binary differences
-  if (is.null(binary_vars) == FALSE){
-  binary_vars_2 <- paste0(binary_vars, "_binary_diff")
-  binary_sum_df <- lapply(binary_vars_2, FUN = function(x) {
-    binary_summary(x, nn_diff_df = nn_diff_df, n = n)
-  }) %>% do.call(cbind.data.frame, .)
-  comps_sum <- cbind(comps_sum, binary_sum_df)
+  if (is.null(binary_vars) == FALSE) {
+    binary_vars_names <- paste0(binary_vars, "_binary_diff")
+    binary_sum_df <- lapply(binary_vars_names, FUN = function(x) {
+      binary_summary(x, nn_diff_df = nn_diff_df, n = n)
+    }) %>% do.call(cbind.data.frame, .)
+    comps_sum <- cbind(comps_sum, binary_sum_df)
   }
 
   # Continuous
-  if (is.null(cont_vars) == FALSE){
-  if (log_2 == TRUE) {
-    cont_vars2_2 <- c(paste0(cont_vars, "_diff"), paste0(cont_vars, "_log_2_diff"))
-    cont_sum_df <- lapply(cont_vars2_2, FUN = function(x) {
-      cont_summary(x, nn_diff_df = nn_diff_df, n = n)
-    }) %>% do.call(cbind.data.frame, .)
-  } else {
-    cont_vars2_2 <- c(paste0(cont_vars, "_diff"))
-    cont_sum_df <- lapply(cont_vars2_2, FUN = function(x) {
-      cont_summary(x, nn_diff_df = nn_diff_df, n = n)
-    }) %>% do.call(cbind.data.frame, .)
+  if (is.null(continuous_vars) == FALSE) {
+    if (log_2 == TRUE) {
+      continuous_vars_names <- c(paste0(continuous_vars, "_diff"), paste0(continuous_vars, "_log_2_diff"))
+      cont_sum_df <- lapply(continuous_vars_names, FUN = function(x) {
+        cont_summary(x, nn_diff_df = nn_diff_df, n = n)
+      }) %>% do.call(cbind.data.frame, .)
+    } else {
+      continuous_vars_names <- c(paste0(continuous_vars, "_diff"))
+      cont_sum_df <- lapply(continuous_vars_names, FUN = function(x) {
+        cont_summary(x, nn_diff_df = nn_diff_df, n = n)
+      }) %>% do.call(cbind.data.frame, .)
   }
     comps_sum <- cbind(comps_sum, cont_sum_df)
   }
@@ -58,7 +58,7 @@ cat_summary <- function(var, nn_diff_df, n) {
   return(data)
 }
 
-binary_summary <- function(var, nn_diff_df , n) {
+binary_summary <- function(var, nn_diff_df, n) {
   gain_prop <- sum(nn_diff_df[, var] == "gain") / n
   loss_prop <- sum(nn_diff_df[, var] == "loss") / n
   data <- cbind.data.frame(gain_prop, loss_prop) %>% `colnames<-`(paste0(var,  c("_gain_prop", "_loss_prop")))
