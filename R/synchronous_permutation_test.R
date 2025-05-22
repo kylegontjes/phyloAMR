63f680f460aaced4f3fad602e98fe6d746b66be0
kylegontjes <kgontjes@umich.edu>
2025-05-22 09:38:57 -0400

    update name of synchronous test and streamline it a little

--- R/synchronous_permutation_test.R
+++ R/synchronous_permutation_test.R
@@ -15,20 +15,21 @@
 #' @return Synchronous gain and loss events of two traits with p-value permutation testing results
 #' @export
 synchronous_permutation_test <- function(comparitor, df, tr, tip_name_variable, trait, node_states = "joint", confidence_threshold = NULL, confidence = NULL, num_permutations = 1000, num_cores = 1) {
-  df <- df %>% .[match(tr$tip.label, .[[tip_name_variable]]), ]
+  df <- df[match(tr$tip.label, df[[tip_name_variable]]), ]
   tr$node.label <- NULL
 
   # Trait
-  trait_asr <- asr(df = df, tr = tr, tip_name_variable = tip_name_variable, trait = trait, model = "ER", node_states = node_states, confidence_threshold = confidence_threshold) %>% .$parent_child_df
+  trait_asr <- asr(df = df, tr = tr, tip_name_variable = tip_name_variable, trait = trait, model = "ER", node_states = node_states, confidence_threshold = confidence_threshold)
+  trait_asr_parent_child <- trait_asr$parent_child_df
 
   # Comparitor
   comparitor_asr <- asr(df = df, tr = tr, tip_name_variable = tip_name_variable, trait = comparitor, model = "ER", node_states = node_states, confidence_threshold = confidence_threshold)
   comparitor_asr_parent_child <- comparitor_asr$parent_child_df
 
   # Observed data
-  trait_synchronous_obs <- synchronous_detection(comparitor_asr_parent_child, trait_asr, node_states = node_states, confidence = confidence)
+  trait_synchronous_obs <- synchronous_transitions(comparitor_asr_parent_child, trait_asr_parent_child, node_states = node_states, confidence = confidence)
 
-  if(trait_synchronous_obs$synchronous_transitions_num == 0) {
+  if (trait_synchronous_obs$synchronous_transitions_num == 0) {
     trait_synchronous_obs$transition_pval <-  1
     trait_synchronous_obs$synchronous_gain_pval <-  1
     trait_synchronous_obs$synchronous_loss_pval <-  1
@@ -39,24 +40,27 @@ synchronous_permutation_test <- function(comparitor, df, tr, tip_name_variable, 
 
   # Generate rate data
   comparitor_index_mat <- comparitor_asr$corHMM_out$index.mat
-  comparitor_p <- sapply(1:max(comparitor_index_mat, na.rm = TRUE), function(x)
-      na.omit(c(comparitor_asr$corHMM_out$solution))[na.omit(c(comparitor_index_mat) == x)][1])
+  comparitor_p <- sapply(1:max(comparitor_index_mat, na.rm = TRUE), function(x) {
+    na.omit(c(comparitor_asr$corHMM_out$solution))[na.omit(c(comparitor_index_mat) == x)][1]
+    })
 
   # Expected data from permutation testing
   num_isolates <- nrow(df)
   comparitor_vals <- df[[comparitor]]
-  trait_runs <- replicate(num_permutations,comparitor_vals[sample.int(num_isolates)],simplify = F)
+  trait_runs <- replicate(num_permutations, comparitor_vals[sample.int(num_isolates)], simplify = FALSE)
   tip_names <- df[[tip_name_variable]]
 
   # Permutations
-  asr_permutation <- mclapply(permutation_names, FUN = function(x) {
-    dataset <- cbind(tip_names,x)
-    outcome_str <- setNames(x,tip_names)
-    asr_recon <- ancRECON(tr,dataset,p = comparitor_p, method = node_states, rate.cat = 1, rate.mat = comparitor_asr$corHMM_out$index.mat, root.p = comparitor_asr$corHMM_out$root.p, get.likelihood = F, get.tip.states = F)
-    asr_result <- get_parent_child_data(tr = tr,ancestral_states = asr_recon$lik.anc.states,trait_data = outcome_str,confidence_threshold = confidence_threshold,node_states = node_states) %>% get_continuation_data(.,'joint')
-    comparitor_sychronous_perm <- synchronous_detection(asr_result, trait_asr)
+  asr_permutation <- parallel::mclapply(trait_runs, FUN = function(x) {
+    dataset <- cbind(tip_names, x)
+    outcome_str <- setNames(x, tip_names)
+    asr_recon <- corHMM::ancRECON(tr, dataset, p = comparitor_p, method = node_states, rate.cat = 1, rate.mat = comparitor_asr$corHMM_out$index.mat, root.p = comparitor_asr$corHMM_out$root.p, get.likelihood = FALSE, get.tip.states = FALSE)
+    asr_parent_child <- get_parent_child_data(tr = tr, ancestral_states = asr_recon$lik.anc.states, trait_data = outcome_str, confidence_threshold = confidence_threshold, node_states = node_states)
+    asr_result <- get_continuation_data(parent_child_df = asr_parent_child, node_states = node_states)
+    comparitor_sychronous_perm <- synchronous_transitions(comparitor_parent_child_df = asr_result, trait_parent_child_df =  trait_asr_parent_child, node_states = node_states, confidence = confidence)
     return(comparitor_sychronous_perm)
-  }, mc.cores = num_cores) %>% do.call(rbind, .)
+  }, mc.cores = num_cores)
+  asr_permutation <- do.call(rbind, asr_permutation)
 
   # Calculate p-values for each event of interest and add them to the trait observation data
   trait_synchronous_obs$transition_pval <- c(1 + sum(asr_permutation$synchronous_transitions_num >= trait_synchronous_obs$synchronous_transitions_num)) / c(1 + num_permutations)
@@ -66,6 +70,8 @@ synchronous_permutation_test <- function(comparitor, df, tr, tip_name_variable, 
   trait_synchronous_obs$synchronous_loss_gain_pval <- c(1 + sum(asr_permutation$synchronous_loss_gain_num >= trait_synchronous_obs$synchronous_loss_gain_num)) / c(1 + num_permutations)
 
   }
+
   results <- list(synchronous_permutation_testing = trait_synchronous_obs, permutations = asr_permutation)
+
   return(results)
 }
--- R/synchronous_transitions.R
+++ R/synchronous_transitions.R
@@ -1,4 +1,4 @@
-#' synchronous_detection: Synchronous detection
+#' Analysis of synchronous transitions of two traits
 #'
 #' Synchronous gain and loss of two traits at a spot on tree
 #'
@@ -8,44 +8,58 @@
 #' @param confidence Whether to use high or low confidence transition nodes when node_states are marginal
 #' @return Synchronous gain and loss events of two traits
 #' @export
-synchronous_detection <- function(comparitor_parent_child_df, trait_parent_child_df, node_states = "joint", confidence = NULL) {
+synchronous_transitions <- function(comparitor_parent_child_df, trait_parent_child_df, node_states = "joint", confidence = NULL) {
+  # Get gain and loss nodes for the comparitor and trait
   if (node_states == "joint") {
-    trait_gains <- trait_parent_child_df %>% subset(gain == 1) %>% .$child
-    comparitor_gains <- comparitor_parent_child_df  %>% subset(gain == 1) %>% .$child
-    trait_losses <- trait_parent_child_df %>% subset(loss == 1) %>% .$child
-    comparitor_losses <- comparitor_parent_child_df  %>% subset(loss == 1) %>% .$child
+    trait_gains <- trait_parent_child_df[trait_parent_child_df$gain == 1, "child"]
+    comparitor_gains <- comparitor_parent_child_df[comparitor_parent_child_df$gain == 1, "child"]
+    trait_losses <- trait_parent_child_df[trait_parent_child_df$loss == 1, "child"]
+    comparitor_losses <- comparitor_parent_child_df[comparitor_parent_child_df$loss == 1, "child"]
   } else {
-    trait_gains <- trait_parent_child_df %>% subset(get(paste0("gain_", confidence)) == 1) %>% .$child
-    comparitor_gains <- comparitor_parent_child_df  %>% subset(get(paste0("gain_", confidence)) == 1) %>% .$child
-    trait_losses <- trait_parent_child_df %>% subset(get(paste0("loss_", confidence)) == 1) %>% .$child
-    comparitor_losses <- comparitor_parent_child_df  %>% subset(get(paste0("loss_", confidence)) == 1) %>% .$child
+    # Get variable to consider
+    gain_variable <- ifelse(confidence == "high", "gain_high", "gain")
+    loss_variable <- ifelse(confidence == "high", "loss_high", "loss")
+    # Identify
+    trait_gains <- trait_parent_child_df[trait_parent_child_df[[gain_variable]] == 1, "child"]
+    comparitor_gains <- comparitor_parent_child_df[comparitor_parent_child_df[[gain_variable]] == 1, "child"]
+    trait_losses <- trait_parent_child_df[trait_parent_child_df[[loss_variable]] == 1, "child"]
+    comparitor_losses <- trait_parent_child_df[trait_parent_child_df[[loss_variable]] == 1, "child"]
   }
+
   # Trait
   num_trait_gains <- length(trait_gains)
   num_trait_losses <- length(trait_losses)
 
   # Synchronous gains
-  synchronous_gains  <- trait_gains[which(trait_gains %in%  comparitor_gains)] %>% as.numeric %>% sort
+  synchronous_gains  <- trait_gains[which(trait_gains %in% comparitor_gains)]
+  synchronous_gains <- sort(as.numeric(synchronous_gains))
   synchronous_gains_str <-  if (length(synchronous_gains) == 0) "" else paste0(synchronous_gains, collapse = ",")
   synchronous_gains_num <- length(synchronous_gains)
-  synchronous_gains_prop <- synchronous_gains_num / num_trait_gains
+  synchronous_gains_prop <- round(synchronous_gains_num / num_trait_gains, 2)
+
   # Synchronous gains and losses
-  synchronous_gain_loss <- trait_gains[which(trait_gains %in%  comparitor_losses)] %>% as.numeric %>% sort
+  synchronous_gain_loss <- trait_gains[which(trait_gains %in%  comparitor_losses)]
+  synchronous_gain_loss <- sort(as.numeric(synchronous_gain_loss))
   synchronous_gain_loss_str <- if (length(synchronous_gain_loss) == 0) "" else paste0(synchronous_gain_loss, collapse = ",")
   synchronous_gain_loss_num <- length(synchronous_gain_loss)
-  synchronous_gain_loss_prop <- synchronous_gain_loss_num / num_trait_gains
+  synchronous_gain_loss_prop <- round(synchronous_gain_loss_num / num_trait_gains, 2)
+
   # Synchronous losses
-  synchronous_losses <- trait_losses[which(trait_losses %in%  comparitor_losses)] %>% as.numeric %>% sort
+  synchronous_losses <- trait_losses[which(trait_losses %in%  comparitor_losses)]
+  synchronous_losses <- sort(as.numeric(synchronous_losses))
   synchronous_losses_str <- if (length(synchronous_losses) == 0) "" else paste0(synchronous_losses, collapse = ",")
   synchronous_losses_num <- length(synchronous_losses)
-  synchronous_losses_prop <- synchronous_losses_num / num_trait_losses
+  synchronous_losses_prop <- round(synchronous_losses_num / num_trait_losses, 2)
+
   # Synchronous loss gain
-  synchronous_loss_gain <- trait_losses[which(trait_losses %in%  comparitor_gains)] %>% as.numeric %>% sort
+  synchronous_loss_gain <- trait_losses[which(trait_losses %in%  comparitor_gains)]
+  synchronous_loss_gain <- sort(as.numeric(synchronous_loss_gain))
   synchronous_loss_gain_str <- if (length(synchronous_loss_gain) == 0) "" else paste0(synchronous_loss_gain, collapse = ",")
   synchronous_loss_gain_num <- length(synchronous_loss_gain)
-  synchronous_loss_gain_prop <- synchronous_loss_gain_num / num_trait_losses
+  synchronous_loss_gain_prop <- round(synchronous_loss_gain_num / num_trait_losses, 2)
+
   # Number synchronous transitions
-  synchronous_transitions_num <- sum(synchronous_gains_num, synchronous_gain_loss_num , synchronous_losses_num ,synchronous_loss_gain_num)
+  synchronous_transitions_num <- sum(synchronous_gains_num, synchronous_gain_loss_num, synchronous_losses_num, synchronous_loss_gain_num)
 
   summary <- data.frame(num_trait_gains, synchronous_transitions_num, synchronous_gains = synchronous_gains_str, synchronous_gains_num, synchronous_gains_prop,
                         synchronous_gain_loss = synchronous_gain_loss_str, synchronous_gain_loss_num, synchronous_gain_loss_prop,
